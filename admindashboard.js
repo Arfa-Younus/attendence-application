94a4621bce00062158123404fd4783e433d205b4
Arfa-Younus <arfayounus99@gmail.com>
2023-09-11 14:24:30 +0500

    updated

--- admindashboard.js
+++ admindashboard.js
@@ -108,3 +108,78 @@ const app = firebase.initializeApp(firebaseConfig);
 function submitData() {
     window.location.href = "admindashboard.html";
 }
+
+// ******************validation of form*****************
+
+function submitData() {
+  // Read form values
+  readFom();
+
+  // Perform form validation
+  let isValid = true;
+
+  // Regular expressions for validation
+  const nameRegex = /^[A-Za-z\s]+$/;
+  const rollNumberRegex = /^[A-Za-z0-9\s]+$/;
+  const contactNumberRegex = /^[0-9]{10}$/;
+  const cnicNumberRegex = /^[0-9]{13}$/;
+
+  if (!nameRegex.test(nameV)) {
+    isValid = false;
+    document.getElementById("name-error").textContent = "Invalid name format";
+  } else {
+    document.getElementById("name-error").textContent = "";
+  }
+
+  if (!rollNumberRegex.test(rollNumberV)) {
+    isValid = false;
+    document.getElementById("rollNumber-error").textContent = "Invalid roll number format";
+  } else {
+    document.getElementById("rollNumber-error").textContent = "";
+  }
+
+  if (!contactNumberRegex.test(contactNumberV)) {
+    isValid = false;
+    document.getElementById("contactNumber-error").textContent = "Invalid contact number format";
+  } else {
+    document.getElementById("contactNumber-error").textContent = "";
+  }
+
+  if (!cnicNumberRegex.test(cnicNumberV)) {
+    isValid = false;
+    document.getElementById("cnicNumber-error").textContent = "Invalid CNIC number format";
+  } else {
+    document.getElementById("cnicNumber-error").textContent = "";
+  }
+
+  if (isValid) {
+    // Submit the data to Firebase
+    firebase
+      .database()
+      .ref("teacher/" + nameV)
+      .set({
+        nameNo: nameV,
+        fatherName: fatherNameV,
+        rollNumber: rollNumberV,
+        contactNumber: contactNumberV,
+        cnicNumber: cnicNumberV,
+        picture: pictureV,
+        courseName: courseNameV,
+        assignedClass: assignedClassV,
+      });
+    alert("Data Inserted");
+    
+    // Clear the form fields
+    document.getElementById("name").value = "";
+    document.getElementById("fatherName").value = "";
+    document.getElementById("rollNumber").value = "";
+    document.getElementById("contactNumber").value = "";
+    document.getElementById("cnicNumber").value = "";
+    document.getElementById("picture").value = "";
+    document.getElementById("courseName").value = "";
+    document.getElementById("assignedClass").value = "";
+  } else {
+    // Alert the user that there are validation errors
+    alert("Please fix the validation errors and try again.");
+  }
+}
